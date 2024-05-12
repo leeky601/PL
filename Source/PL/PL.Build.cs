4264a1357135393ca17a3cf85f9f4f3a460b1eca
leeky601 <leeky601@naver.com>
2024-05-12 22:20:46 +0900

    dfdf
    dsfdfdf

---
+++ Source/PL/PL.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class PL : ModuleRules
+{
+	public PL(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
